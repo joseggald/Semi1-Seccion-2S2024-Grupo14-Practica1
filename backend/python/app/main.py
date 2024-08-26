09c323eef1cd1cdbfc42d77a954c7a8df81e5370
joseggald <josegald49@gmail.com>
2024-08-25 23:36:38 -0600

    [UPDATE] Se agrego el cors que faltaba para peticiones de cliente

--- backend/python/app/main.py
+++ backend/python/app/main.py
@@ -2,14 +2,24 @@ from fastapi import FastAPI
 from app.api import users
 from app.db import base, session
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI( title = settings.PROJECT_NAME)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
 
 @app.on_event("startup")
 def on_startup():
     base.Base.metadata.create_all(bind=session.engine)
+
 
 if __name__ == "__main__":
     import uvicorn
